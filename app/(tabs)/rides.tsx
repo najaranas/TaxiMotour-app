5c41ed6c67a877b64cb4c68694ea3d949585ec68
Najar Anas <anasnajar.tn@gmail.com>
2025-07-23 23:46:29 +0100

    commit

--- app/(tabs)/rides.tsx
+++ app/(tabs)/rides.tsx
@@ -8,11 +8,11 @@ import { useTheme } from "@/contexts/ThemeContext";
 export default function RidesScreen() {
   const { theme } = useTheme();
   return (
-    <ScreenWrapper>
+    <ScreenWrapper scroll safeArea padding={horizontalScale(20)}>
       <View style={styles.container}>
         <View style={styles.header}>
           <View style={{ gap: verticalScale(5) }}>
-            <Typo variant="body">Rides History</Typo>
+            <Typo variant="h3">Rides History</Typo>
             <Typo variant="body" color={theme.text.secondary}>
               Showing all your rides
             </Typo>
@@ -25,6 +25,55 @@ export default function RidesScreen() {
             />
           </View>
         </View>
+        <View style={{ gap: verticalScale(10) }}>
+          <Typo size={moderateScale(20)} variant="h3">
+            Active rides
+          </Typo>
+          <View
+            style={{
+              flexDirection: "row",
+              alignContent: "center",
+              gap: horizontalScale(20),
+              justifyContent: "space-between",
+              // backgroundColor: "#f5f5f53f",
+              borderRadius: theme.borderRadius.large,
+              borderWidth: theme.borderWidth.thin,
+              borderColor: theme.gray.border,
+              padding: horizontalScale(20),
+            }}>
+            <View></View>
+            <View
+              style={{
+                justifyContent: "space-between",
+                gap: verticalScale(20),
+              }}>
+              <View style={{ gap: verticalScale(5), alignItems: "center" }}>
+                <Typo variant="caption" color={theme.text.muted}>
+                  Payment
+                </Typo>
+                <View
+                  style={{
+                    borderRadius: theme.borderRadius.pill,
+                    backgroundColor: "#ffb2183d",
+                    paddingInline: horizontalScale(10),
+                    paddingBlock: horizontalScale(5),
+                  }}>
+                  <Typo variant="caption" color={theme.text.secondary}>
+                    TND13
+                  </Typo>
+                </View>
+              </View>
+              <View style={{ gap: verticalScale(5), alignItems: "center" }}>
+                <Typo variant="caption" color={theme.text.muted}>
+                  Distance
+                </Typo>
+                <Typo variant="caption" color={theme.text.secondary}>
+                  12Km
+                </Typo>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </ScreenWrapper>
   );
@@ -33,9 +82,7 @@ export default function RidesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
+    gap: verticalScale(30),
   },
   header: {
     flexDirection: "row",
---
+++ components/Ride/Ride.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Ride() {
+  return (
+    <View>
+      <Text>Ride</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
